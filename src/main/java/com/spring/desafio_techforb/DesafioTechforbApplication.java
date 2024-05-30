83df54f6c41425c4a5d6e181a2b14337334429e1
Joaquin <joaquinmouxaut@gmail.com>
2024-05-30 23:05:49 +0200

    first commit

---
+++ src/main/java/com/spring/desafio_techforb/DesafioTechforbApplication.java
@@ -0,0 +1,13 @@
+package com.spring.desafio_techforb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTechforbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTechforbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/desafio_techforb/DesafioTechforbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.desafio_techforb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTechforbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
